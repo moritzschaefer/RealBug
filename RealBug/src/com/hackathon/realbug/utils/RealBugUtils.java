c08cda8f42f06dbc354a1c65892afeab705ce54c
Moritz Schäfer <mollitz@gmail.com>
2012-12-02 04:47:58 +0100

    Push java

--- RealBug/src/com/hackathon/realbug/utils/RealBugUtils.java
+++ RealBug/src/com/hackathon/realbug/utils/RealBugUtils.java
@@ -1,4 +1,4 @@
-package com.hackathon.realbug.utils;
+package com.hackathon.realbugtest;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,6 +18,7 @@ import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,7 +33,7 @@ public class RealBugUtils {
     public static final String REAL_BUG_JSON_KEY_DESCRIPTION = "description";
     public static final String REAL_BUG_JSON_KEY_IMAGE = "image";
     public static final String REAL_BUG_JSON_KEY_ID = "id";
-    public static final String URL = "obscure-fjord-9100.herokuapp.com";
+    public static final String URL = "http://obscure-fjord-9100.herokuapp.com/index.php";
     
     public static final String LOG_TAG = "RealBugUtils";
 
@@ -40,7 +41,7 @@ public class RealBugUtils {
     /**
      * Container class for one entry in the RealBug-list
      */
-    public class RealBugData {
+    public static class RealBugData {
         public double longitude;
         public double latitude;
         public String description;
@@ -70,7 +71,7 @@ public class RealBugUtils {
      * @param description   The description from the user for the RealBug
      * @return              Returns a json-object as String 
      */
-    private String jsonStringCreate(double longitude, double latitude, String description) {
+    private static String jsonStringCreate(double longitude, double latitude, String description) {
         JSONObject object = new JSONObject();
         try {
             object.put(REAL_BUG_JSON_KEY_POSITION, String.format("%f,%f", longitude, latitude));
@@ -90,7 +91,7 @@ public class RealBugUtils {
      * @param image         The image the user took to document the RealBug as JPG
      *
      */
-    public void realBugPost(double longitude, double latitude, String description, byte[] image) {
+    public static void realBugPost(double longitude, double latitude, String description, byte[] image) {
         String jsonPostObject = jsonStringCreate(longitude, latitude, description);
         DefaultHttpClient httpclient = new DefaultHttpClient(); 
         HttpPost httppost = new HttpPost(URL+"/RealBug");
@@ -134,7 +135,9 @@ public class RealBugUtils {
         }
         JSONObject obj;
         try {
-        	obj = new JSONObject(convertStreamToString(inputStream));
+        	String returnValue = convertStreamToString(inputStream);
+        	Log.d(LOG_TAG, returnValue);
+        	obj = new JSONObject(returnValue);
             updateRealBugImg(obj.getInt(REAL_BUG_JSON_KEY_ID), image);
         }
         catch(JSONException e) {
@@ -143,7 +146,6 @@ public class RealBugUtils {
         }
 
     }
-
     /**
      * Get all RealBugs in radius around given position and return as a list 
      * 
@@ -154,10 +156,12 @@ public class RealBugUtils {
      * @return List of all RealBugs with max-distance radius from current position
      */
 
-    public List<RealBugData> realBugAreaList(double longitude, double latitude, double radius) {
+    public static List<RealBugData> realBugAreaList(double longitude, double latitude, double radius) {
         DefaultHttpClient httpclient = new DefaultHttpClient(); 
-        HttpGet httpget = new HttpGet(URL+String.format("/RealBug/%f,%f,%f",longitude, latitude, radius));
-
+        //TODO make this with HttpParams !!!!
+        HttpGet httpget = new HttpGet(URL+String.format("/RealBug?ln=%f&lt=%f&rt=%f",longitude, latitude, radius));
+        
+        
         httpget.setHeader("Accept", "application/json");
         //httpget.setHeader("Content-Type", "application/json");
         HttpResponse response;
@@ -194,7 +198,7 @@ public class RealBugUtils {
 
         
 
-        List<RealBugData> returnArray = new ArrayList<RealBugData>(jsonResults.length());
+        List<RealBugData> returnArray = new ArrayList<RealBugData>();
         for(int i=0; i<jsonResults.length(); i++) {
         	int id;
         	String position, description;
@@ -208,12 +212,76 @@ public class RealBugUtils {
         		return new ArrayList<RealBugUtils.RealBugData>();
         	}
             String image = String.format("/RealBug/%d/img", id);
-            returnArray.set(i, new RealBugData(position, description, image));
+            returnArray.add(new RealBugData(position, description, image));
         }
         return returnArray;
     }
 
-    public RealBugData realBugById(int id) {
+    /**
+     * Get all RealBugs 
+     * 
+     * @return List of all RealBugs 
+     */
+
+    public static List<RealBugData> realBugList() {
+        DefaultHttpClient httpclient = new DefaultHttpClient(); 
+        HttpGet httpget = new HttpGet(URL+"/RealBug");
+        httpget.setHeader("Accept", "application/json");
+        //httpget.setHeader("Content-Type", "application/json");
+        HttpResponse response;
+
+        try {
+            response = httpclient.execute(httpget);
+        }
+        catch (Exception e) {
+            //Filter e for 400 (client error) 500 (user error) 300 (forward... change to code to automatically follow :))
+            e.printStackTrace();
+            return new ArrayList<RealBugData>();
+            //Do some user feedback to singalize the fail
+        }
+        HttpEntity entity = response.getEntity();
+        if(entity == null) 
+            return new ArrayList<RealBugData>(); //Return empty list
+        JSONArray jsonResults;
+        try {
+        	InputStream instream = entity.getContent();
+        
+
+        	jsonResults = new JSONArray(convertStreamToString(instream));
+        }
+        catch(IOException e) {
+        	e.printStackTrace();
+        	return new ArrayList<RealBugData>();
+        }
+        catch(JSONException e) {
+        	e.printStackTrace();
+        	return new ArrayList<RealBugUtils.RealBugData>();
+        }
+
+        httpget.abort();
+
+        
+
+        List<RealBugData> returnArray = new ArrayList<RealBugData>();
+        for(int i=0; i<jsonResults.length(); i++) {
+        	int id;
+        	String position, description;
+        	try {
+	            JSONObject jsonObject = jsonResults.getJSONObject(i);
+	            description =  jsonObject.getString(REAL_BUG_JSON_KEY_DESCRIPTION);
+	            position =  jsonObject.getString(REAL_BUG_JSON_KEY_POSITION);
+	            id =  jsonObject.getInt(REAL_BUG_JSON_KEY_ID);
+        	} catch(JSONException e) {
+        		e.printStackTrace();
+        		return new ArrayList<RealBugUtils.RealBugData>();
+        	}
+            String image = String.format(URL+"/RealBug/%d/img", id);
+            returnArray.add(new RealBugData(position, description, image));
+        }
+        return returnArray;
+    }
+
+    public static RealBugData realBugById(int id) {
         DefaultHttpClient httpclient = new DefaultHttpClient(); 
         HttpGet httpget = new HttpGet(URL+String.format("/RealBug/%d", id));
 
@@ -254,7 +322,7 @@ public class RealBugUtils {
         }
     }
 
-    public void updateRealBugImg(int id, byte[] image) {
+    public static void updateRealBugImg(int id, byte[] image) {
         DefaultHttpClient httpclient = new DefaultHttpClient(); 
         HttpPut httpput = new HttpPut(URL+String.format("/RealBug/%d/img",id));
 
@@ -273,7 +341,7 @@ public class RealBugUtils {
         }
     }
 
-    public byte[] realBugImage(int id) {
+    public static byte[] realBugImage(int id) {
         DefaultHttpClient httpclient = new DefaultHttpClient(); 
         HttpGet httpget = new HttpGet(URL+String.format("/RealBug/%d/img", id));
 
